1d17d90c295c6b1e28fd4e85bfa51b0137524e21
elijah12era-ux <elijah12era@gmail.com>
2025-12-02 10:46:51 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: "/future-vision-hq/",
   server: {
     host: "::",
     port: 8080,
